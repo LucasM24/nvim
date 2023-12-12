80f12df9bf06134aa732f2691b99c4d202d3dde6
Lucas Mendez-dev <lucas.mendez@est.fi.uncoma.edu.ar>
2023-12-12 15:36:52 -0300

    Add - Treesitter

---
+++ lua/config/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,   -- make sure we load this during startup if it is your main colorscheme
+  config = function()
+    require 'nvim-treesitter.configs'.setup {
+    
+      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+      ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "typescript", "http", "json", "php", "css", "scss", "vue" },
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = true,
+
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = false,
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+
+      indent = {
+        enable = true
+      }
+    }
+  end
+}
