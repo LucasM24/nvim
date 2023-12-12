eedbf3313dac0b375f1469f852807bc64ade05f3
Lucas Mendez-dev <lucas.mendez@est.fi.uncoma.edu.ar>
2023-12-11 22:15:04 -0300

    first configuration

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,55 @@
+-- Keymaps --
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Leader key
+vim.g.mapleader = ' '
+
+-- Normal --
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Remap save command
+keymap('n', '<Leader>w', ':write<CR>',{})
+
+-- Remap quit command
+-- keymap('n', '<Leader>q', ':bd<CR>',{})
+-- keymap('n', '<Leader>q', ':q<CR>',{})
+
+-- Center the view when search something in the file
+keymap('n', 'n', 'nzzzv', {})
+keymap('n', 'N', 'Nzzzv', {})
+
+-- Insert --
+
+-- Remap for ESC key
+keymap("i", "jk", "<ESC>", {})
+
+-- Visual --
+
+--Still in visual mode when indenting code
+keymap('v', '<', '<gv', {})
+keymap('v', '>', '>gv', {})
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+vim.cmd[[vnoremap // y/\V<C-R>=escape(@",'/\')<CR><CR>]]
---
+++ lua/core/options.lua
@@ -0,0 +1,37 @@
+local set = vim.opt
+
+vim.g.mapleader = " "
+
+-- Sets config
+set.nu = true -- Show the number of the lines
+set.relativenumber = false -- Set relative numbers
+set.wrap = false -- Deactivate the line wrapping
+set.fileencoding = 'utf-8'
+set.termguicolors = true
+set.hidden = true
+set.backup = false
+set.swapfile = false
+set.autoindent = true
+set.smartindent = true
+set.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+set.mouse = "a"
+
+-- Search settings
+set.hlsearch = false
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+-- Windows settings
+set.splitbelow = true
+set.splitright = true
+set.scrolloff = 4
+
+-- Highligth on yank
+vim.cmd[[augroup highlight_yank
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank { higroup="Visual", timeout=300 }
+augroup END]]
+
+-- Avoid comment in next line
+vim.cmd([[autocmd FileType * set formatoptions-=ro]])
