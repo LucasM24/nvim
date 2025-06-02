c5246b9052e3e47681e1553913418c5596230b94
Lucas Mendez <lucas.mendez@est.fi.uncoma.edu.ar>
2025-06-02 17:17:29 -0300

    Exec typst server v1

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -7,15 +7,34 @@ set.shiftwidth = 2
 set.formatoptions = 'tqj'
 set.textwidth = 80
 
+local exec = function()
+  local filename = vim.fn.expand("%:t")
+  local i, j = string.find(filename, ".typ")
+  local pdf = string.sub(filename, 1, i) .. "pdf"
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "*.typ",
-  group = vim.api.nvim_create_augroup("Typst group", {clear = true}),
-  callback = function()
-    local name = vim.fn.expand("%:t")
-    -- vim.print(name)
-    job = vim.system({"typst", "watch", name}, {}, function()
-      -- vim.print("Process ended", out.code)
-    end)
-  end
-})
+  vim.system({"typst", "watch", filename}, {}, function()
+    vim.print("Process ended", out.code)
+  end)
+  vim.system({"zathura", pdf}, {}, function()
+    vim.print("Process ended", out.code)
+  end)
+end
+
+-- vim.api.nvim_set_keymap("n", "<leader>x", exec, {noremap = true})
+vim.keymap.set({"n"}, "<leader>x", exec)
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = "*.typ",
+--   group = vim.api.nvim_create_augroup("Typst group", {clear = true}),
+--   callback = function()
+--     local filename = vim.fn.expand("%:t")
+--     local i, j = string.find(filename, ".typ")
+--     local pdf = string.sub(filename, 1, i) .. "pdf"
+--
+--     vim.system({"typst", "watch", filename}, {}, function()
+--       vim.print("Process ended", out.code)
+--     end)
+--     vim.system({"mupdf", pdf}, {}, function()
+--       vim.print("Process ended", out.code)
+--     end)
+--   end
+-- })
